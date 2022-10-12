1ddc31c038456a6508ee2f0483b791ee81ba54fe
Al Amin Eimon <eimonalamin@gmail.com>
2022-10-12 22:49:40 +0600

    Feature added

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -3,13 +3,13 @@ import React from "react";
 function Feature() {
   return (
     <div className=" lg:flex px-6 bg-green-200 py-1">
-      <div className="lg:h-60 mr-16 sm:w-100 lg:w-3/12">
+      {/* <div className="lg:h-60 mr-16 sm:w-100 lg:w-3/12">
         <img
           className="h-full sm:ml-16"
           src="https://appsgeyser.com/blog/wp-content/uploads/2020/09/Artboard-92.png"
           alt=""
         />
-      </div>
+      </div> */}
       <div className=" py-2 mt-2 lg:w-9/12">
         <h1 className="">
           <strong>1</strong> A multiple choice quiz maker for memory retention
