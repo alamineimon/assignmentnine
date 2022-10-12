f85ca503c55a2bbe2ebce6b2df57b6029758f7b4
Al Amin Eimon <eimonalamin@gmail.com>
2022-10-12 22:44:55 +0600

    Quiz added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,15 +3,7 @@ import { useLoaderData } from 'react-router';
 import QuizData from '../QuizData/QuizData';
 
 function Quiz() {
-    const loedQuiz = useLoaderData().data
-    console.log(loedQuiz);
-    return (
-      <div className="grid gap-12 md:grid-cols-3 sm:grid-cols-2">
-        {loedQuiz.questions.map((quizData) => (
-          <QuizData key={loedQuiz.id} quizData={quizData}></QuizData>
-        ))}
-      </div>
-    );
+
 }
 
 export default Quiz;
